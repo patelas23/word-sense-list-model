731a4f0ebdea533c697b78806c23e6aaae05bd67
Ankur Patel <patelas23@vcu.edu>
2022-03-23 17:49:01 -0400

    initial commit

---
+++ wsd.py
@@ -0,0 +1,14 @@
+# Author: Ankur Patel
+# Course: CMSC-416-001-SP2022 Natural Language Processing
+# Instructor: Bridgette Mckinnes
+
+# A Python script which implements a Decision List classifier to 
+#   perform word-sense disambiguation
+# Usage: python3 wsd.py training.txt test.txt model.txt > answers.txt
+
+import sys
+
+
+
+if __name__ == "__main__":
+    print('Welcome to wsd.py!')
