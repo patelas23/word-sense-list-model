22280eeeb777bc6616538dc9a463d612034b83bf
Ankur Patel <patelas23@vcu.edu>
2022-03-26 13:49:59 -0400

    file parsing

--- wsd.py
+++ wsd.py
@@ -7,8 +7,21 @@
 # Usage: python3 wsd.py training.txt test.txt model.txt > answers.txt
 
 import sys
+import xml.etree.ElementTree as ET
+import pandas as pd
 
+def parse_text(corpus_string):
+    return pd.read_json(corpus_string)
 
+def learn_model():
+    pass
 
 if __name__ == "__main__":
     print('Welcome to wsd.py!')
+    training_file = sys.argv[1]
+    training_corpus_string = ""
+    
+    with open(training_file) as file:
+        training_corpus_string = file.read()
+        
+    print(parse_text(training_corpus_string))
