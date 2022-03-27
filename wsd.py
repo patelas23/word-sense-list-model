7b2a5f7e5eab2f99d84df5545faf0db76dec467d
Ankur Patel <patelas23@vcu.edu>
2022-03-27 11:04:56 -0400

    use regex for input file parsing

--- wsd.py
+++ wsd.py
@@ -5,21 +5,35 @@
 # A Python script which implements a Decision List classifier to 
 #   perform word-sense disambiguation
 # Usage: python3 wsd.py training.txt test.txt model.txt > answers.txt
+from html.parser import HTMLParser
 
 import sys
 import pandas as pd
 import xml.etree.ElementTree as ET
 import re
 
+# Helper class for parsing input text
 def parse_text(corpus_string):
-    corpus_string = re.sub(r'<@>', ' ', corpus_string)
-    corpus_tree = ET.ElementTree(corpus_string)
-    print(corpus_tree)
+    sense_tagger = re.compile(r'(senseid=")(\S+)("/>)')
+    head_tagger = re.compile(r'(<head>)(\S+)(</head>)')
+    context_tagger = re.compile(r'(<context>\n)(.*)(\n</context>)')
+    # Extract each line
+        # Extract each sense 
+        # Extract the head, 
+        # Extract each surrounding word and add it to count
+    # Produce log probabilities
+    pass
 
 def learn_model():
     # Extract each head and associate it with its sense
     # Extract words surrounding head and count their overall frequency
     pass
+
+# Read input file, extract each head and disambiguate it
+# For each <head> word, generate log probability 
+def apply_model():
+    pass
+
 
 if __name__ == "__main__":
     print('Welcome to wsd.py!')
@@ -28,6 +42,11 @@ if __name__ == "__main__":
     
     with open(training_file) as file:
         training_corpus_string = file.read()
-        
+    
     parse_text(training_corpus_string)
+    
+    # Attempt to create html parser 
+    # parser = WordSenseParser()
+    # parser.feed(training_corpus_string)
+        
         
